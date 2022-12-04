6aac4ec8eac6113757cbbfbc8e2301eca19bb82c
Adevair Vitório <62258557+adevairvitorio@users.noreply.github.com>
2022-12-04 10:20:22 -0300

    initial

---
+++ assets/js/load_manager.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#header-content").load("pages/application/header.html");
+    $("#footer-content").load("pages/application/footer.html");
+    $("#main-menu-content").load("pages/sections/main/main_menu.html");
+
+});
